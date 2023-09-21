10b4dcae75bd87f028e1c2ca26c6efb2ad0330d1
ToluAjisola <toluwalopeajisola@gmail.com>
2023-09-20 23:47:44 -0400

    Exercise Details added

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { exerciseOptions, fetchData, youtubeOptions } from "../utils/fetchData";
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
 
 const ExerciseDetail = () => {
   return (
-    <div>ExerciseDetail</div>
-  )
-}
+    <Box sx={{ mt: { lg: "96px", xs: "60px" } }}>
+      <Detail  />
+      <ExerciseVideos
+        />
+      <SimilarExercises
+       />
+    </Box>
+  );
+};
 
-export default ExerciseDetail
+export default ExerciseDetail;

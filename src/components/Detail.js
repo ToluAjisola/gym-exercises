fe3026c5895714237c2fd6bf93b8f0368f5db379
ToluAjisola <toluwalopeajisola@gmail.com>
2023-09-21 23:24:46 -0400

    Implement Details section

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Typography, Stack, Button } from "@mui/material";
 
-const Detail = () => {
+import BodyPartImage from "../assets/icons/body-part.png";
+import TargetImage from "../assets/icons/target.png";
+import EquipmentImage from "../assets/icons/equipment.png";
+
+const Detail = ({ exerciseDetail }) => {
+  const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+
   return (
-    <div>Detail</div>
-  )
-}
+    <Stack
+      gap="60px"
+      sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}
+    >
+      <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+      <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
+        <Typography
+          sx={{ fontSize: { lg: "64px", xs: "30px" } }}
+          fontWeight={700}
+          textTransform="capitalize"
+        >
+          {name}
+        </Typography>
+        <Typography
+          sx={{ fontSize: { lg: "24px", xs: "18px" } }}
+          color="#4F4C4C"
+        >
+          Exercises keep you strong.{" "}
+          <span style={{ textTransform: "capitalize" }}>{name}</span> bup is one
+          of the best <br /> exercises to target your {target}. It will help you
+          improve your <br /> mood and gain energy.
+        </Typography>
+        
+      </Stack>
+    </Stack>
+  );
+};
 
-export default Detail
+export default Detail;

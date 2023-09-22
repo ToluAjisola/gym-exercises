30fc6204e10fbfd00c179bdb3483d8c13edb1847
ToluAjisola <toluwalopeajisola@gmail.com>
2023-09-21 22:46:46 -0400

    Fetch data added

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -8,13 +8,33 @@ import ExerciseVideos from "../components/ExerciseVideos";
 import SimilarExercises from "../components/SimilarExercises";
 
 const ExerciseDetail = () => {
+const [exerciseDetail, setExerciseDetail] = useState({});
+const { id } = useParams();
+
+   useEffect(() => {
+     window.scrollTo({ top: 0, behavior: "smooth" });
+
+     const fetchExercisesData = async () => {
+       const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+       const youtubeSearchUrl =
+         "https://youtube-search-and-download.p.rapidapi.com";
+
+       const exerciseDetailData = await fetchData(
+         `${exerciseDbUrl}/exercises/exercise/${id}`,
+         exerciseOptions
+       );
+       setExerciseDetail(exerciseDetailData);
+
+     };
+     fetchExercisesData();
+   }, [id]);
+
+  
   return (
     <Box sx={{ mt: { lg: "96px", xs: "60px" } }}>
-      <Detail  />
-      <ExerciseVideos
-        />
-      <SimilarExercises
-       />
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
     </Box>
   );
 };

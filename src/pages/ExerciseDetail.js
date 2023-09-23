89764282b116f113787652ca4399f6e31be708c0
ToluAjisola <toluwalopeajisola@gmail.com>
2023-09-22 23:39:57 -0400

    Similar Exercise fetched

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -11,6 +11,7 @@ const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
   const [exerciseVideos, setExerciseVideos] = useState([]);
   const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+  const [equipmentExercises, setEquipmentExercises] = useState([]);
 const { id } = useParams();
 
    useEffect(() => {
@@ -33,6 +34,18 @@ const { id } = useParams();
        );
        setExerciseVideos(exerciseVideosData.contents);
 
+       const targetMuscleExercisesData = await fetchData(
+         `${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`,
+         exerciseOptions
+       );
+       setTargetMuscleExercises(targetMuscleExercisesData);
+
+       const equimentExercisesData = await fetchData(
+         `${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`,
+         exerciseOptions
+       );
+       setEquipmentExercises(equimentExercisesData);
+
      };
      fetchExercisesData();
    }, [id]);
@@ -45,7 +58,10 @@ const { id } = useParams();
         exerciseVideos={exerciseVideos}
         name={exerciseDetail.name}
       />
-      <SimilarExercises />
+      <SimilarExercises
+        targetMuscleExercises={targetMuscleExercises}
+        equipmentExercises={equipmentExercises}
+      />
     </Box>
   );
 };

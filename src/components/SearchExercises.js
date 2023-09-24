73ce5baf4501c14e5f4f2b0a72c368ad95c4b2dc
ToluAjisola <toluwalopeajisola@gmail.com>
2023-09-23 22:43:51 -0400

    Similar exercises component implemented

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -95,9 +95,9 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
       <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
         <HorizontalScrollbar
           data={bodyParts}
-          bodyParts
-          setBodyPart={setBodyPart}
           bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+          isBodyParts
         />
       </Box>
     </Stack>
